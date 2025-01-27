3c3781f97a18f2b90d9bac3df853db6468ad78c1
Sayed Haider <sayedhaider2002@gmail.com>
2025-01-27 22:41:28 +0300

    This is the intro to express lab

---
+++ server.js
@@ -0,0 +1,102 @@
+// Import Express
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+
+// Create an Express app
+const app = express()
+
+
+// Define routes here (we'll add them soon)
+
+//========================================================================
+//1. Be Polite, Greet the User
+
+app.get('/greetings/:username', (req, res) => {
+    res.send(`Hello there, ${req.params.username}`);
+});
+
+//========================================================================
+//2. Rolling the Dice
+
+app.get('/roll/:numberParam', (req, res) => {
+    const number = req.params.numberParam;
+
+    if(!isNaN(number)){
+    res.send(`You rolled a ${number}`);
+
+    }
+    else {
+        res.send('Invalid number');
+    }
+})
+
+//========================================================================
+//3. Rolling the Dice
+
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+  
+app.get('/collectibles/:indexParam', (req, res) => {
+
+    if (req.params.indexParam > 2 ) {
+        res.send('This item is not yet in stock. Check back soon!') 
+    }
+    else{
+        res.send(`So, you want the ${collectibles[req.params.indexParam].name}? For ${collectibles[req.params.indexParam].price}, it can be yours!`);
+    }   
+})
+
+//========================================================================
+//4. Filter Shoes by Query Parameters
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+app.get('/shoes', (req, res) => {
+
+    const min = req.query.minPrice;
+    const max = req.query.maxPrice;
+    const shoeType = req.query.type;
+
+    const result = []; 
+
+for (let i = 0; i < shoes.length; i++) {
+
+const shoe = shoes[i];
+        if (shoe.price >= min) {
+            result.push(shoe);
+        }
+        if (shoe.type === shoeType){
+            result.push(shoe);
+        }
+        if (shoe.price <= max){
+            result.push(shoe);
+        }
+        // else{
+        //     res.send('Nothing Matches!');
+        // }
+    }
+        res.send(result);
+});
+
+
+
+
+
+
+
+// Listen for requests on port 4000
+app.listen(4000, () => {
+    console.log('Listening on port 4000')
+  })
